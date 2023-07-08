ef6cda039a1e270474e1c1653973f08bbb1c26a2
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-07-08 18:14:22 +0100

    Implement (broken) miniMax function

--- script2.js
+++ script2.js
@@ -5,6 +5,7 @@ const player = (counter) => {
 const gameController = (function(){
     let board = ['', '', '', '', '', '', '', '', ''];
     let gameOver = false;
+    const display = document.querySelector('.display');
     const players = [player('X'), player('O')];
 
     const init = () => {  //this function basically initialises everything.
@@ -16,22 +17,37 @@ const gameController = (function(){
                 let spaceOccupied = false;
                 if (board[e.target.dataset.key] != '') spaceOccupied = true;
                 if (spaceOccupied == false && !gameOver){
-                    counterPlacer(e.target.dataset.key, players[0].counter);
-                     //play move
+                    playRound(e.target.dataset.key, players[0].counter);
+                    if (!gameOver) playRound(AI.moveSelector(board), players[1].counter);
                 }
             })
         })
      }
 
-     const counterPlacer = (position, counter) => {
+     const playRound = (position, counter) => {
         board[position] = counter;
         console.log(victoryChecker(board));
+        let winner = victoryChecker(board);
+        if (winner != null) {
+            gameOver = true;
+            if (winner == 'X') display.textContent = 'X victory!';
+            if (winner == 'O') display.textContent = 'O victory!';
+            if (winner == 'draw') display.textContent = 'Draw!';
+
+        }
         displayController.divWriter(board);
      }
 
      const victoryChecker = (board) => {
         let winner = null;
-        console.log('init');
+        let freePositions = 0;
+        // check for draw
+        for (let i = 0; i < 9; i++) {
+            if (board[i] == '') {
+                freePositions++;
+            }
+        }
+        if (freePositions == 0) winner = 'draw';
         const checkIfEqual = (x, y, z) => {return (board[x] == board[y] && board[y] == board[z] && board[x] != '')};
         for (let i = 0; i < 9; i += 3) { //check horizontals
             if (checkIfEqual(i, i+1, i+2)) winner = board[i];
@@ -39,12 +55,12 @@ const gameController = (function(){
         for (let i = 0; i < 3; i++) { //check verticals
             if (checkIfEqual(i, i+3, i+6)) winner = board[i];
         }
-        //checking diagonals
-        if (checkIfEqual(0, 4, 8) || checkIfEqual(2, 4, 6)) winner = board[4];
+        if (checkIfEqual(0, 4, 8) || checkIfEqual(2, 4, 6)) winner = board[4]; //checking diagonals
+
         return winner;
      };
 
-     return {init};
+     return {init, victoryChecker, board};
 })();
 
 let displayController = (function() {
@@ -58,5 +74,60 @@ let displayController = (function() {
     }
     return{divWriter};
 })();
+
+const AI = (function() {
+
+    const moveSelector = (board) => {
+        let highestScore = -100000000;
+        let move;
+        for (let i = 0; i < 9; i++) {
+            if (board[i] == '') {   //check for occupied space
+                board[i] = 'O';
+                let score = miniMax(board, 0, true);
+                if (score > highestScore) { //works out which move produces the highest score.
+                    highestScore = score;
+                    move = i;
+                }
+                board[i] = '';
+            }
+        }
+        console.log(move);
+        return move;
+    }
+
+    const miniMax = (board, depth, isMaximising) => {
+        let tempBoard = board;
+        if (gameController.victoryChecker(board) == 'O') return 10;
+        if (gameController.victoryChecker(board) == 'draw') return 0;
+        if (gameController.victoryChecker(board) == 'X') return -10;
+        
+        if (isMaximising) {
+            let bestScore = -100000000;
+            for (let i = 0; i < 9; i++) {
+                if (tempBoard[i] == '') {
+                    tempBoard[i] = 'X';
+                    let score = miniMax(tempBoard, depth + 1, false);
+                    tempBoard[i] = '';
+                    bestScore = Math.max(score, bestScore);
+                } 
+            }
+            return bestScore;
+        }
+        if (!isMaximising){
+            let bestScore = 1000000000;
+            for (let i = 0; i < 9; i++) {
+                if (tempBoard[i] == '') {
+                    tempBoard[i] = 'O';
+                    let score = miniMax(tempBoard, depth + 1, true);
+                    tempBoard[i] = ''
+                    bestScore = Math.min(bestScore, score);
+                    // console.log(depth);
+                } 
+            }
+            return bestScore;
+        }
+    }
+return{moveSelector};
+})()
 
 gameController.init();
